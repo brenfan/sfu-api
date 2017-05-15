3665b58c3c33d2f8031da60ccab86775be8d0be3
brendan <bca60@sfu.ca>
2017-05-15 01:57:26 -0700

    finished a few stubs

---
+++ python/roads.py
@@ -0,0 +1,18 @@
+
+#Module for reading from the SFU Road Conditions API
+#Returns pretty formatted plaintext strings for whatever use.
+#API URL: http://www.sfu.ca/security/sfuroadconditions/api/2/current
+
+
+def get():
+    response = requests.get("http://www.sfu.ca/security/sfuroadconditions/api/2/current")
+    
+
+
+def announcements():
+
+
+
+
+
+def roads.conditions(campus = "burnaby"):
